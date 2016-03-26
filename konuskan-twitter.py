9d2329f328888adce2292109f1d9e359daf642b2
iamdual <2@kodigen.com>
2016-03-26 23:25:51 +0000

    gunc

--- konuskan-twitter.py
+++ konuskan-twitter.py
@@ -8,6 +8,7 @@ CONSUMER_KEY = 'OndA1IbisgwyqflyNfqVSZyMX'
 CONSUMER_SECRET = 'ZzNxanCvO3sAz78htAdCY3JDLJzzLZNu9KJyVzzkPNP4eEYM7c'
 ACCESS_TOKEN = '713839272646692868-HIDDEN-'
 ACCESS_TOKEN_SECRET = '-HIDDEN-'
+TWITTER_USERNAME = 'konuskanbot'
 
 class konuskan_twitter:
     def __init__(self):
@@ -28,11 +29,12 @@ class konuskan_twitter:
                 sender = dm['sender']['screen_name']
                 text   = dm['text']
                 
-                print "Gonderen: @%s\nMesaj:\n%s\n" % (sender, text)
-                
-                cikis = zeka.__init__(text)
-                if cikis:
-                    twitter_api.direct_messages.new(user=sender, text=cikis)
+                if text and sender != TWITTER_USERNAME:
+                    print "Sender: @%s\nMessage: %s\n" % (sender, text)
+
+                    cikis = zeka.__init__(text)
+                    if cikis:
+                        twitter_api.direct_messages.new(user=sender, text=cikis)
         
 if __name__ == "__main__":
     konuskan_twitter()
